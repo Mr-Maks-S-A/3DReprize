d71b83a185178dffa6e437579954c5302d7df680
Mr-Maks-S-A <mrfarkses@gmail.com>
2025-06-02 22:21:18 +0300

    Added simple FindGlm.cmake

--- Cmake/FindGLM.cmake
+++ Cmake/FindGLM.cmake
@@ -1,20 +1,43 @@
-set(GLM_SEARCH_PATHS ${DEPENDENCIES_PATH} ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
+set(GLM_SEARCH_PATHS    "${DEPENDENCIES_PATH}/glm"
+                        ${glm_SOURCE_DIR}
+                        ${CMAKE_INSTALL_INCLUDEDIR}
+                        ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
+                        )
 
 find_path(GLM_INCLUDE_PATH
-    NAMES glm/glm.hpp
+    NAMES glm/glm.hpp glm/gtx
     PATHS ${GLM_SEARCH_PATHS}
-    PATH_SUFFIXES include)
+    PATH_SUFFIXES include
+    )
 
-find_library(GLM_LIBRARIES
-    NAMES glm libglm.a
+
+if(${CMAKE_BUILD_TYPE} STREQUAL Debug)
+find_library(GLM_LIBRARIES_Debug
+    NAMES libglm.a libglm.lib
     PATHS ${GLM_SEARCH_PATHS}
-    PATH_SUFFIXES lib)
+    PATH_SUFFIXES ${CMAKE_BUILD_TYPE}
+    )
+    SET(GLM_LIBRARIES ${GLM_LIBRARIES_Debug})
+endif()
 
 
-set(GLM_FOUND NOT)
+if(${CMAKE_BUILD_TYPE} STREQUAL Release)
+find_library(GLM_LIBRARIES_Release
+    NAMES libglm.a libglm.lib
+    PATHS ${GLM_SEARCH_PATHS}
+    PATH_SUFFIXES ${CMAKE_BUILD_TYPE}
+    )
+SET(GLM_LIBRARIES ${GLM_LIBRARIES_Release})
+endif()
+
+
+set(GLM_FOUND OFF)
+
 if (GLM_INCLUDE_PATH AND GLM_LIBRARIES)
-    set(GLM_FOUND TRUE)
-    message("EXTERNAL LIBRARY 'GLM' FOUND")
+    set(GLM_FOUND ON)
+    message(STATUS "SUCCESS: EXTERNAL LIBRARY 'GLM' FOUND")
+    message(STATUS "GLM_INCLUDEPATH = ${GLM_INCLUDE_PATH}")
+    message(STATUS "GLM_LIBRARIES = ${GLM_LIBRARIES}")
 else ()
-    message("ERROR: EXTERNAL LIBRARY 'GLM' NOT FOUND")
+    message(STATUS "ERROR: EXTERNAL LIBRARY 'GLM' NOT FOUND")
 endif (GLM_INCLUDE_PATH AND GLM_LIBRARIES)
